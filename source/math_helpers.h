649ee1d99c4121305b84c44f10d21cb1e0cb9821
Juanes Rayo Work <lufi2544@gmail.com>
2025-03-19 10:24:48 +0100

    Added cross product and dot product

--- source/math_helpers.h
+++ source/math_helpers.h
@@ -21,13 +21,7 @@ typedef struct
 
 /////// VECTOR ///////
 
-internal void
-vec2_print(vec2_t *vec)
-{
-    assert(vec != 0);
-    printf("x: %.2f; y: %.2f \n", vec->x, vec->y);
-}
-
+///// V3 //////
 internal void 
 vec3_print(vec3_t vec)
 {
@@ -43,24 +37,20 @@ project_vec3(vec3_t point, f32 fov)
     return projected_point;
 }
 
-
-internal void 
-vec2_multiply_scalar(vec2_t *point, f32 scalar)
-{
-    assert(point != 0);
-    
-    point->x *= scalar;
-    point->y *= scalar;
-}
-
 internal void
 vec3_multiply_scalar(vec3_t *point, f32 scalar)
-{
-    assert(point != 0);
-    
+{    
     point->x *= scalar;
     point->y *= scalar;
     point->z *= scalar;
+}
+
+internal void
+vec3_divide_scalar(vec3_t *v, f32 s)
+{
+	v->x /= s;
+	v->y /= s;
+	v->z /= s;
 }
 
 internal vec3_t
@@ -102,7 +92,6 @@ vec3_rotate_z(vec3_t v, f32 angle)
     return rotated_vector;
 }
 
-
 internal f32 
 vec3_length(vec3_t v)
 {
@@ -111,6 +100,44 @@ vec3_length(vec3_t v)
 	return result;
 }
 
+internal vec3_t
+vec3_subtract(vec3_t a, vec3_t b)
+{
+	vec3_t result;
+	result.x = a.x - b.x;
+	result.y = a.y - b.y;	
+	return result;
+}
+
+internal vec3_t
+vec3_cross(vec3_t a, vec3_t b)
+{
+	vec3_t result = 
+	{
+		result.x = (a.y * b.z) - (a.z * b.y),		
+		result.y = (a.z * b.x) - (a.x * b.z),	
+		result.z = (a.x * b.y) - (a.y - b.x)		
+	};
+	
+	return result;
+}
+
+internal f32 vec3_dot(vec3_t a, vec3_t b)
+{
+	f32 result = 0;	
+	result = a.x * b.x + a.y * b.y;	
+	return result;
+}
+
+
+////// VEC2 //////
+
+internal void
+vec2_print(vec2_t *vec)
+{
+    assert(vec != 0);
+    printf("x: %.2f; y: %.2f \n", vec->x, vec->y);
+}
 
 internal f32 
 vec2_length(vec2_t v)
@@ -118,4 +145,30 @@ vec2_length(vec2_t v)
 	f32 result;	
 	result = sqrt((v.x * v.x) + (v.y * v.y));	
 	return result;
+}
+
+internal void 
+vec2_multiply_scalar(vec2_t *point, f32 scalar)
+{
+    assert(point != 0);
+    
+    point->x *= scalar;
+    point->y *= scalar;
+}
+
+internal vec2_t
+vec2_subtract(vec2_t a, vec2_t b)
+{
+	vec2_t result;
+	result.x = a.x - b.x;
+	result.y = a.y - b.y;	
+	return result;
+}
+
+
+internal void
+vec2_divide_scalar(vec2_t *v, f32 s)
+{
+	v->x /= s;
+	v->y /= s;
 }
